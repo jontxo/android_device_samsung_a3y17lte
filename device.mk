07b5007d18364db469b18add82f053d4b2b8a4b6
McFy49 <g.matthieu49@gmail.com>
2018-06-28 17:35:05 +0200

    a3y17lte: Add audio package

--- device.mk
+++ device.mk
@@ -75,3 +75,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_gains.xml:system/etc/mixer_gains.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+<<<<<<< HEAD
+=======
+# Audio Package
+PRODUCT_PACKAGES += \
+   libtfa98xx \
+   libtinycompress
+>>>>>>> f852af9... a3y17lte: Add audio package
