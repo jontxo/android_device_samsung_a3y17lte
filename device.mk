449d09a2b16196e584338713756b71e8d543b8ee
McFy49 <g.matthieu49@gmail.com>
2018-06-29 16:43:01 +0200

    a3y17lte: Add HIDL target

--- device.mk
+++ device.mk
@@ -50,6 +50,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf \
     wificond \
+    android.hardware.wifi@1.0-service
 
 # Wi-Fi Configs
 PRODUCT_COPY_FILES += \
@@ -107,7 +108,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     mcDriverDaemon \
     fingerprint.exynos5 \
-    libbauthtzcommon_shim
+    libbauthtzcommon_shim \
+    android.hardware.biometrics.fingerprint@2.1-impl \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 
 ###########
 #  Light  #
@@ -115,6 +119,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     lights.universal7870 \
+    android.hardware.light@2.0-impl
 
 
 #########
